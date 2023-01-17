df5c4146eae431dd82fdf5a95b80e98e2424badc
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-18 02:14:27 +0900

    部分テンプレートと映画登録画面の作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,17 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      redirect_to @movie
+    else
+      render "new"
+    end
+  end
 end
