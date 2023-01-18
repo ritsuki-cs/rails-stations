7720e8df9579e78786583db11e400b543c3faada
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-19 01:05:30 +0900

    バリデーションの追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 160 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 160 }
   validates :year, length: { maximum: 45 }
   validates :image_url, length: { maximum: 150 }
   validates :is_showing, inclusion: {in: [true, false]}
+  validates :description, format: { with: /\A.*\z/, multiline: true }
 end
