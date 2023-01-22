886096ebeda07362f7b1dfc2cabaf9cd1436cf3c
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-22 14:08:46 +0900

    検索機能の実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,35 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @isCheckedAll = false
+    @isChecked1 = false
+    @isChecked0 = false
+    if params[:keyword]
+      if params[:is_showing] == "1"
+        @isChecked1 = true
+        @movies = Movie.where("name like '%#{params[:keyword]}%'")
+                  .or(Movie.where("description like '%#{params[:keyword]}%'"))
+                  .where(is_showing: true)
+      elsif params[:is_showing] == "0"
+        @isChecked0 = true
+        @movies = Movie.where("name like '%#{params[:keyword]}%'")
+                  .or(Movie.where("description like '%#{params[:keyword]}%'"))
+                  .where(is_showing: false)
+      else
+        @isCheckedAll = true
+        @movies = Movie.where("name like '%#{params[:keyword]}%'")
+                  .or(Movie.where("description like '%#{params[:keyword]}%'"))
+      end
+    else
+      if params[:is_showing] == "1"
+        @isChecked1 = true
+        @movies = Movie.where(is_showing: true)
+      elsif params[:is_showing] == "0"
+        @isChecked0 = true
+        @movies = Movie.where(is_showing: false)
+      else
+        @isCheckedAll = true
+        @movies = Movie.all
+      end
+    end
   end
 end
